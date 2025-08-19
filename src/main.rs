7914aae1773a59221c8e5c30ae5540fd6b9bce33
JuhoErvasti <juho.ervasti@gispo.fi>
2025-08-19 21:01:13 +0300

    Handle joining event thread

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ use cli_log::*;
 
 use crossterm::event::{DisableMouseCapture, EnableMouseCapture};
 use tat::dataset::{DatasetRequest, DatasetResponse, TatDataset};
-use std::sync::mpsc::{self, SendError};
-use std::thread;
+use std::sync::mpsc::{self, SendError, TryRecvError};
+use std::thread::{self};
+use std::time::Duration;
 use std::{env::temp_dir, fs::File};
 use clap::{CommandFactory, Parser};
 
@@ -27,18 +28,30 @@ struct Cli {
     all_drivers: bool,
 }
 
-fn handle_events(tx: mpsc::Sender<TatEvent>) -> Result<(), SendError<TatEvent>> {
+fn handle_events(tx: mpsc::Sender<TatEvent>, rx: mpsc::Receiver<bool>) -> Result<(), SendError<TatEvent>> {
     loop {
-        // TODO: don't unwrap
-        match crossterm::event::read().unwrap() {
-            crossterm::event::Event::Key(key_event) => {
-                tx.send(TatEvent::Keyboard(key_event))?
-            },
-            crossterm::event::Event::Mouse(mouse_event) => {
-                tx.send(TatEvent::Mouse(mouse_event))?
-            },
-            _ => {},
+        let poll_result = crossterm::event::poll(Duration::from_millis(50));
+        if let Ok(res) = poll_result {
+            if res {
+                match crossterm::event::read().unwrap() {
+                    crossterm::event::Event::Key(key_event) => {
+                        tx.send(TatEvent::Keyboard(key_event))?
+                    },
+                    crossterm::event::Event::Mouse(mouse_event) => {
+                        tx.send(TatEvent::Mouse(mouse_event))?
+                    },
+                    _ => (),
+                }
+            }
         }
+
+        match rx.try_recv() {
+            Ok(_) | Err(TryRecvError::Disconnected) => {
+                return Ok(());
+            }
+            Err(TryRecvError::Empty) => {},
+        }
+
     }
 }
 
@@ -64,6 +77,7 @@ fn main() {
 
     let (dataset_request_tx, dataset_request_rx) = mpsc::channel::<DatasetRequest>();
     let (tatevent_tx, tatevent_rx) = mpsc::channel::<TatEvent>();
+    let (event_thread_tx, event_thread_rx) = mpsc::channel();
 
     // has to be cloned here because it's used later and moved to a closure here
     let cp_tatevent_tx = tatevent_tx.clone();
@@ -113,14 +127,21 @@ fn main() {
     crossterm::execute!(std::io::stdout(), EnableMouseCapture).unwrap();
 
     let event_handle = thread::spawn(move || {
-        handle_events(tatevent_tx).unwrap();
+        match handle_events(tatevent_tx, event_thread_rx) {
+            Ok(_) => (),
+            Err(_) => (),
+        }
     });
 
     let _result = TatApp::new(dataset_request_tx.clone())
         .run(&mut terminal, tatevent_rx);
 
-    // FIXME: handle joining this
-    // event_handle.join().unwrap();
+    match event_thread_tx.send(true) {
+        Ok(_) => (),
+        Err(err) => error!("Could not send signal to terminate event loop {}", err.to_string()),
+    }
+
+    event_handle.join().unwrap();
     ds_handle.join().unwrap();
 
     // FIXME: if the program panics or is killed this will not happen
